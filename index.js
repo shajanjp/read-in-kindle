029518d88567a2c0ee30872b98542b08d5ea37a7
shajanjp <shajanjp@gmail.com>
2021-02-24 22:26:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const https = require("https");
+const { Readability } = require("@mozilla/readability");
+const { JSDOM } = require('jsdom');
+
+function getPageContent(url) {
+  const proto = url.charAt(4) == "s" ? https : http;
+
+  return new Promise((resolve, reject) => {
+    proto
+      .get(url, (res) => {
+        let data = "";
+
+        res.setEncoding("utf8");
+        res
+          .on("data", function (chunk) {
+            data += chunk;
+          })
+          .on("end", () => {
+            resolve(data);
+          });
+      })
+      .on("error", (e) => {
+        reject(e.message);
+      });
+  });
+}
+
+(async () => {
+    const content = await getPageContent('https://nodejs.org/api/http.html');
+    const document = new JSDOM(content);
+    const article = new Readability(document.window.document).parse();
+})()
